0d6e76f509d6c3f4ae2eda508531bf5d55a13a31
dheerajaraj <dheerajaraj@yahoo.com.sg>
2017-05-06 13:44:09 +0800

    sign up page

---
+++ sign_up_page.php
@@ -0,0 +1,22 @@
+<h4>Join us as a Premium User and get access to signing up for SoC events.</h4>
+[pmpro_levels]
+</ br>
+</ br>
+</ br>
+
+<h3>Sign up as premium user and enjoy these privileges!</h3>
+</ br>
+[one_fourth]
+<h4>Promotions: </h4>
+<img src="https://cdn0.iconfinder.com/data/icons/business-and-finance-6/155/vector_285_16-01-512.png" alt="https://cdn0.iconfinder.com/data/icons/business-and-finance-6/155/vector_285_16-01-512.png" />
+[/one_fourth]
+
+[one_fourth]
+<h4>RSVP for premium events: </h4>
+<img src="http://www.troyag.com/wp/wp-content/uploads/2015/07/special_event.png" alt="http://www.troyag.com/wp/wp-content/uploads/2015/07/special_event.png" />
+[/one_fourth]
+
+[one_fourth]
+<h4>Get special discounts: </h4>
+<img src="http://www.webhostingreviewjam.com/wp-content/uploads/2014/04/tag-icon.png" alt="http://www.webhostingreviewjam.com/wp-content/uploads/2014/04/tag-icon.png" />
+[/one_fourth]
